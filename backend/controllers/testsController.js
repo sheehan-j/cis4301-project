23f61e17fb52c187cee71b1c311ba345e76f2805
sheehan-j <jordansheehan@ufl.edu>
2023-03-30 18:52:54 -0400

    Update testsController

--- backend/controllers/testsController.js
+++ backend/controllers/testsController.js
@@ -1,11 +1,38 @@
 const oracledb = require("oracledb");
 const executeQuery = require("../util/executeQuery");
+const translateLabels = require("../util/translateLabels");
 
 exports.getTest = async (req, res) => {
-	console.log("GET TEST");
 	try {
-		const result = await executeQuery("SELECT * FROM test");
-		return res.status(200).json(result);
+		const result = await executeQuery(`
+      SELECT month, SUM(deaths) as deaths
+      FROM Deaths
+      WHERE condition = 3
+      AND year = 2021
+      AND state = 40
+      GROUP BY month
+    `);
+
+		// Capture the values and labels of each record into lists
+		const values = [];
+		result.map((item) => {
+			values.push(item.DEATHS);
+		});
+		const labels = [];
+		result.map((item) => {
+			labels.push(item.MONTH);
+		});
+
+		// Translate month numbers into text
+		const translatedLabels = translateLabels(labels);
+
+		// Create new object
+		const data = {
+			labels: translatedLabels,
+			values: values,
+		};
+
+		return res.status(200).json(data);
 	} catch (err) {
 		return res.status(400).json({ message: err.message });
 	}
