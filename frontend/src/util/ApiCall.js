82bea883a41aa539e51be0fd1db3d46c8ede33de
sheehan-j <jordansheehan@ufl.edu>
2023-04-02 15:17:13 -0400

    Rework ApiCall util function

---
+++ frontend/src/util/ApiCall.js
@@ -0,0 +1,44 @@
+const AveragesApi = require("../api/AveragesApi");
+
+const ApiCall = async (grouping, granularity, minDate, maxDate) => {
+	let result;
+
+	switch (grouping) {
+		case "age":
+			result = await AveragesApi.getDataByAgeGroup(
+				granularity,
+				minDate,
+				maxDate
+			);
+			break;
+		case "condition":
+			result = await AveragesApi.getDataByCondition(
+				granularity,
+				minDate,
+				maxDate
+			);
+			break;
+		case "conditiongroup":
+			result = await AveragesApi.getDataByConditionGroup(
+				granularity,
+				minDate,
+				maxDate
+			);
+			break;
+		case "state":
+			result = await AveragesApi.getDataByState(
+				granularity,
+				minDate,
+				maxDate
+			);
+			break;
+		default:
+			return null;
+	}
+
+	return result;
+};
+
+module.exports = {
+	ApiCall,
+};
