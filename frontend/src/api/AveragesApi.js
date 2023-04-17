b58cc99c3e5f2364680cb37e42df6262099f8678
Jacob Ho <jlhxc25@gmail.com>
2023-04-17 19:43:34 -0400

    Added getDataByRegion to Averages API

--- frontend/src/api/AveragesApi.js
+++ frontend/src/api/AveragesApi.js
@@ -50,6 +50,19 @@ const getDataByState = async (granularity, minDate, maxDate) => {
 	return result;
 };
 
+const getDatabyRegion = async (granularity, minDate, maxDate) => {
+	const API_URL = `http://localhost:8089/api/state/${granularity}/${minDate}/${maxDate}`;
+
+	const response = await fetch(API_URL, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+	const result = await response.json();
+	return result;
+};
+
 module.exports = {
 	getDataByState,
 	getDataByAgeGroup,
